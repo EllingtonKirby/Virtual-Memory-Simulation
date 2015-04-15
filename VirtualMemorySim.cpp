9d766e7db7448f05de570061f1efa8699caea022
Jonathan Ball <jball2@binghamton.edu>
2015-04-14 23:35:16 -0400

    Finished up Lab 5 - see description
    - Added calls to the start, reference, & terminate functions from the
    driver class
    - Changed the physical memory representation to a vector of frame
    structs
    - Implemented the reference & terminate functions

--- VirtualMemorySim.cpp
+++ VirtualMemorySim.cpp
@@ -1,22 +1,67 @@
 #include "VirtualMemorySimulator.h"
 using namespace std;
+#define FREE_FRAME -1
 
 VirtualMemorySimulator::VirtualMemorySimulator(int num_frames){
 	//initialize physical memory to -1, representing vacant
+	//physical_memory size represents
 	for(int i = 0; i < num_frames; i++){
-		physical_memory.push_back(pair<int, int>(-1,-1));
+		//physical_memory.push_back(pair<int, int>(-1,-1));
+
+		struct frame f(FREE_FRAME);
+		physical_memory.push_back(f);
 	}
 }
 
 void VirtualMemorySimulator::start(int process_id, int size){
 	struct process p1;
-	p1.pid = process_id; //this may be unnecessary
+	//p1.pid = process_id; //this may be unnecessary
 	p1.num_pages = size;
 	for(int i = 0; i < num_pages; i++){
 		//vector pages represents a processes address space
 		//each index represents a page
-		//a value of -1 represents 
+		//a value of -1 represents a null reference
 		p1.pages.push_back(-1);
 	}
 	virtual_memory.push_back(pair<process_id, p1>);
+}
+
+void VirtualMemorySimulator::reference(int pid, int page_number){
+	// Check if the page is not resident
+	bool resident = false;
+	struct process p = virtual_memory[pid];
+	for(auto it : p.pages){
+		if(it == page_number)
+			resident = true;
+	}
+
+	// If the page number is not resident, check for available memory
+	if(!resident){
+		for(int i = 0; i < physical_memory.size(); i++){
+			if(physical_memory[i].pid == FREE_FRAME){
+				p.pages.push_back(i);
+				physical_memory[i].pid = pid;
+				break;
+			}
+		}
+	}
+	else{
+		// Do nothing (for now)
+
+	}
+}
+
+void VirtualMemorySimulator::terminate(int process_id){
+	// Free all frames the process references
+	for(auto f : physical_memory){
+		if(f.pid == process_id){}
+			// Deallocate the frame
+			f.pid = FREE_FRAME;
+		}
+	}
+
+	// Remove the process from the map
+	auto p = virtual_memory.find(process_id);
+	if(p != virtual_memory.end())
+		virtual_memory.erase(p);
 }
--- VirtualMemorySim.h
+++ VirtualMemorySim.h
@@ -2,20 +2,37 @@
 #include <vector>
 #include <utility>
 #include <math.h>
+#define NULL_FRAME -1
 
 class VirtualMemorySimulator{
 	private:
 		struct process{
-			int pid;
+		//	int pid;
 			int num_pages;
 			std::vector<int> pages;
 		};
-		std::vector< std::pair<int, int>> physical_memory;
+
+		// This struct will contain info such as age for LRU, size if needed
+		// and everything else for the page replacement algorithms
+		// frame.pid will be -1 for unallocated frames
+		struct frame{
+			int pid; // The process the frame is allocated to
+			frame(int p)
+			: pid(p){}
+		};
+
+		// The vector of frame structs representing physical memory
+		std::vector<frame> physical_memory;
+
+		// Format for physical_memory is pair<pid, page>
+		//std::vector< std::pair<int, int>> physical_memory;
+
+		// pair format is <pid, process>
 		std::map<int,struct process> virtual_memory;
+
 	public:
 		VirtualMemorySimulator(int num_frames);
 		void start(int pid, int size);
 		void terminate(int pid);
 		void reference(int pid, int page_number);
-
 };
